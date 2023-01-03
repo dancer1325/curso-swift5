534341c427280e72c6074bec670f41e98d3fe393
dancer1325 <alfredotic0809@gmail.com>
2023-01-03 10:58:22 +0100

    refactor(var_let.plyaground): add commentaries and more cases (Section2)

--- 02_var_let.playground/Contents.swift
+++ 02_var_let.playground/Contents.swift
@@ -1,34 +1,48 @@
+// Next import library could be commented, because it's not being used in this playground
 import UIKit
 
-let maximumNumberOfLoginAttempts = 3
+// Unnecessary to end each line with ;
+let maximumNumberOfLoginAttempts = 3;
 
 var currentLoginAttempt = 0
 
+// ,    Declare several variables at the same time
 var x = 0.0, y = 0.0, z = 0.0
 
+// If you just declare without initializing, and you don't indicate the type --> Error
+// var variableNotDeclared;
 
 var welcomeMessage : String
+let constantDeclared : String       // Constants can be also just declared and typed
 
-welcomeMessage = "Hola, ¿que tal?"
 
+welcomeMessage = "Hello, how are you?"
+constantDeclared = "Hello"          // Assign the constant after declaring it
+// constantDeclared = "Bye"            // Error if you try to reassign a constant
+
+print("constantDeclared " + constantDeclared)
+
+// Declare typing several variables at the same time
 var red, green, blue : Double
 
-
+// TODO: Find how to type greek or chinese letters. Not find the way to do it
 let π = 3.141592
 let 你好 : String
+// CTRL + COMMAND + TAB SPACE --> Open emoji list
 var 👽 = "Alien"
 
 print(👽)
 👽 = "Alien 2"
+print(👽)
 
-print("El número de login actual es: \(currentLoginAttempt) de un total disponible de: \(maximumNumberOfLoginAttempts)")
+//    /()  interpolation
+print("Current loggin attemp is: \(currentLoginAttempt) from: \(maximumNumberOfLoginAttempts)")
 
-// Esto es un comentario
+// Commentary line
 
-/* Esto es un comentario
- Ocupa varias líneas
- Y puedo escribir muuucha cosa */
+/* Commentary
+ in several lines */
 
-/* Este es un comentario multilinea
- /* Este es un comentario anidado */
- Esto es otro comentario multilinea*/
+/* Multiline commentary
+ /* Nested commentary */
+ Close of the multiline commentary */
